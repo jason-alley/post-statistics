13375542398c04d1db3ee70931c9cafe1123db3a
Jason Cameron <jasoncameron246@gmail.com>
2023-09-16 21:23:43 -0400

    refactored code to use classes and updated html template

--- post-statsistics.php
+++ post-statsistics.php
@@ -8,15 +8,31 @@
     Author URI:   https://j-cameron.me/
  */
 
-
- add_action('admin_menu', 'post_statistics_menu');
+ class PostStatisticsCountPlugin {
+    function __construct() {
+        add_action('admin_menu', array( $this, 'post_statistics_menu') );    
+    }
 
     function post_statistics_menu() {
-        add_options_page('Post Statistics Settings', 'Post Statistics', 'manage_options', 'post-statistics-settings', 'post_statistics_page');
+        add_options_page('Post Statistics Settings', 'Post Statistics', 'manage_options', 'post-statistics-settings', array( $this, 'post_statistics_page') );
     }
 
     function post_statistics_page() {
         ?>
-        <p>Hello from our pots stats plugin.</p>
+        <div class="wrap">
+            <h1>Post Statistics Settings</h1>
+            <form method="post" action="options.php">
+                <?php
+                    settings_fields('post-statistics-settings');
+                    do_settings_sections('post-statistics-settings');
+                    submit_button();
+                ?>
+            </form>
         <?php
     }
+ }
+
+ $postStatisticsCountPlugin =  new PostStatisticsCountPlugin();
+
+
+    
