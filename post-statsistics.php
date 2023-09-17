68798d8976b07fdf11a6c911ccbbd9e69992a9a8
Jason Cameron <jasoncameron246@gmail.com>
2023-09-16 21:04:06 -0400

    plugin comment block added

---
+++ post-statsistics.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+    Plugin Name:  Post Statistics.
+    Description:  A plugin that lets the reader see stats about a post.
+    Version:      0.1
+    Author:      Jason Cameron
+    Author URI:   https://j-cameron.me/
+ */
