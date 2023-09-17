47d8486ab2cec3fe4de8abb15148d20d4d9e4fee
Jason Cameron <jasoncameron246@gmail.com>
2023-09-16 21:15:43 -0400

    added basic plugin setup and admin page.

--- post-statsistics.php
+++ post-statsistics.php
@@ -7,3 +7,16 @@
     Author:      Jason Cameron
     Author URI:   https://j-cameron.me/
  */
+
+
+ add_action('admin_menu', 'post_statistics_menu');
+
+    function post_statistics_menu() {
+        add_options_page('Post Statistics Settings', 'Post Statistics', 'manage_options', 'post-statistics-settings', 'post_statistics_page');
+    }
+
+    function post_statistics_page() {
+        ?>
+        <p>Hello from our pots stats plugin.</p>
+        <?php
+    }
